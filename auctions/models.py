31c9c7687a1e655aacbe80cbbf22cd6692546ee3
Mouhany <mouhany@gmail.com>
2023-04-05 16:25:23 +0700

    Create models and edit templates

--- auctions/models.py
+++ auctions/models.py
@@ -7,25 +7,31 @@ class User(AbstractUser):
     # watchlist
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f"{self.category}"
+
+
+# class Bid(models.Model):
+#     current_bid = models.FloatField()
+
+#     def __str__(self):
+#         return f"{self.current_bid}"
+    
+    
 class Auction(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     start_bid = models.FloatField()
-    image = models.TextField()
-    # category = models.Choices
+    # current_bid = models.ForeignKey(Bid, on_delete=models.PROTECT, related_name="price")
+    image = models.URLField()
+    # category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name = "group")
     
     def __str__(self):
         return f"ID: {self.id} | Title: {self.title} | Description: {self.description} | Start Bid: {self.start_bid} | Image URL:{self.image}"
-    
-
-
-# class Bid(models.Model):
-#     # current_price = 
-#     # bid_price = 
-#     pass
-
 
 # class Comment(models.Model):
-#     auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_comments")
 #     comment = models.TextField()
 #     date = models.DateTimeField(auto_now=True)
